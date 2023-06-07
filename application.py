90a4f5567b2a3687f8f3944e3b033578ab456070
Haru Im <chalk323@naver.com>
2023-06-07 22:17:37 +0900

    임하루/application.py/오류발생부분코드 삭제 및 api 테스트

--- application.py
+++ application.py
@@ -3,7 +3,7 @@
 #원활한 aws 배포를 위해 포트는 8001으로 통일합니다 (4000, 3000은 피해주세요)
 #커밋 메세지에 무엇을 수정했는지 자세하게 적어주세요(한글로 길게 쓸 수 있어요)
 # 커밋 메세지 통일 : (수정자)/(날짜)/(수정한 파일명)/(수정한 부분)/(수정한 이유)/
-# ex) 박교담/2023.06.06/application.py/getrequests 함수/코드 수정/코드를 간결하게 수정
+# ex) 박교담/2023.06.06/app.py/getrequests 함수/코드 수정/코드를 간결하게 수정
 # 스테이징 시 주의사항 : git add . 하지 말아주세요
 # 스테이징 시 수정할 파일만 뽑아서 스테이징해주세요
 # 커밋 메세지 통일 꼭 지켜주세요
@@ -12,14 +12,14 @@
 # 안녕하십니까~~
 # =======
 # 하이
-from bson import Binary
 
-import requests
-r = requests.get('http://127.0.0.1:5500/shipping99_16/templates/index.html')
-rjson = r.json()
 
-from flask import Flask, render_template
-application = Flask(__name__)
+
+
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
+app= Flask(__name__)
+CORS(app)
 
 
 @app.route("/save_movie", methods=["POST"])
@@ -53,10 +53,17 @@ def back():
     return str(straGram_data)
 
 
-@application.route('/')
+@app.route('/api',methods=['GET'])
+def get_example_data() :
+    data = {
+        "hi" : 123
+    }
+    return jsonify(data)
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    application.run('0.0.0.0', port=8001, debug=True)
+    app.run('0.0.0.0', port=8001, debug=True)
