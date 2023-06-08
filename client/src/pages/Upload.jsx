f1ed6d54a8ea7bd68a2c8f8664c62f8e2d21b7db
Haru Im <chalk323@naver.com>
2023-06-08 13:55:29 +0900

    임하루/PostCard.jsx/이미지 업로드 페이지

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -1,3 +1,48 @@
+import { useCallback, useRef } from "react";
+import axios from "axios";
+
 export const Upload = () => {
-  return <div>Upload</div>;
+  const inputRef = useRef(null);
+  const onUploadImage = useCallback((e) => {
+    if (!e.target.files) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", e.target.files[0]);
+
+    axios({
+      baseURL: "http://localhost:8001/",
+      url: "/api/images",
+      method: "POST",
+      data: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const onUploadImageButtonClick = useCallback(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.click();
+  }, []);
+
+  return (
+    <div>
+      <input type="file" accept="image/*" name="thumbnail" ref={inputRef} />
+      <button label="이미지 업로드" />
+      <button type="submit" onClick={onUploadImageButtonClick}>
+        hi
+      </button>
+      {/* <button label="이미지 제거" onClick={onDeleteImage} /> */}
+    </div>
+  );
 };
